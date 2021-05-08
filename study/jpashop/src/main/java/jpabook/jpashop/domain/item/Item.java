c62641fa98a89213a1253f07846560efea9ff58f
SeungYong Oh <osy090712@gmail.com>
2021-05-08 23:36:02 +0900

    feat - add entity business logic feat - add exception class

--- study/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ study/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==// 도메인 주도 설계라고 할 때 엔티티 자체가 해결 할 수 있는 것들이나 엔티티 안에 비지니스 로직을 넣는게 좋다. 객체지향
+    // 재고수량이 Item 엔티티 안에 있는데 이 데이터를 가지고 있는 거에서 비지니스 로직이 나가는게 응집도가 있기 때문
+    // 데이터를 가지고 있는 쪽에 비지니스 메소드가 있는 게 좋음. 관리의 장점도 있음.
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0)
+            throw new NotEnoughStockException("need more stock");
+        this.stockQuantity = restStock;
+    }
 }
